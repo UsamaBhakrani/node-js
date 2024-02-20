57f9cfd33afc85009987736bbd90191633a2e794
Mohammed Usama <usamabhakrani@gmail.com>
2024-02-20 17:30:04 +0500

    HTTP server

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
-const EventEmitter = require("node:events");
+// const Logger = require("./logger");
+// const logger = new Logger();
+const http = require("node:http");
 
-const Logger = require("./logger");
-const logger = new Logger();
+// logger.on("Logged Message", (e) => {
+//   console.log("Message is:", e);
+// });
 
-logger.on("Logged Message", (e) => {
-  console.log("Message is:", e);
+// logger.log("Message");
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.write("Hello World");
+    res.end();
+  }
+  if (req.url === "/api/courses") {
+    res.write(JSON.stringify([1, 2, 3]));
+    res.end();
+  }
 });
+server.listen(8080);
 
-logger.log("Message");
+console.log("Listening on port 8080");
