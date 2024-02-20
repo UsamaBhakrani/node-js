544be30d77e23fbf29ba4910131589d317afd9fb
UsamaBhakrani <usamabhakrani@gmail.com>
2024-02-21 00:15:04 +0500

    query parameters

--- express/index.js
+++ express/index.js
@@ -8,8 +8,8 @@ app.get("/", (req, res) => {
 app.get("/api/courses", (req, res) => {
   res.send([1, 2, 3, 4, 5, 6, 7, 8, 9]);
 });
-app.get("/*", (req, res) => {
-  res.send("Error Page");
+app.get("/api/course/:id/:year", (req, res) => {
+  res.send(req.query);
 });
 
 const port = process.env.PORT || 3000;
