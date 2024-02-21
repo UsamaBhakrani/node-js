0925377c59521dc74ddcbe2151334983e61b398e
Mohammed Usama <usamabhakrani@gmail.com>
2024-02-21 14:47:02 +0500

    code refactor

--- express/index.js
+++ express/index.js
@@ -30,10 +30,8 @@ app.get("/api/courses/:id", (req, res) => {
 // Post Request
 app.post("/api/courses", (req, res) => {
   const { error } = validateCourse(req.body);
-  if (error) {
-    res.status(400).send(result.error.details[0].message);
-    return;
-  }
+  if (error) return res.status(400).send(result.error.details[0].message);
+
   const course = {
     id: courses.length + 1,
     name: req.body.name,
@@ -45,14 +43,11 @@ app.post("/api/courses", (req, res) => {
 // Put Request
 app.put("/api/courses/:id", (req, res) => {
   const course = courses.find((c) => c.id === parseInt(req.params.id));
-  if (!course) {
-    res.status(404).send("Course does not exist");
-  }
+  if (!course) return res.status(404).send("Course does not exist");
+
   const { error } = validateCourse(req.body);
-  if (error) {
-    res.status(400).send(result.error.details[0].message);
-    return;
-  }
+  if (error) return res.status(400).send(result.error.details[0].message);
+
   course.name = req.body.name;
   res.send(course);
 });
@@ -60,9 +55,8 @@ app.put("/api/courses/:id", (req, res) => {
 // Delete Request
 app.delete("/api/courses/:id", (req, res) => {
   const course = courses.find((c) => c.id === parseInt(req.params.id));
-  if (!course) {
-    res.status(404).send("Course does not exist");
-  }
+  if (!course) return res.status(404).send("Course does not exist");
+
   const index = courses.indexOf(course);
   courses.splice(index, 1);
   res.send(course);
