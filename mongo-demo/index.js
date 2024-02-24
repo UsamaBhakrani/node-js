4b9015c684a9794334f0821938ec21ea600fdc8f
Mohammed Usama <usamabhakrani@gmail.com>
2024-02-24 14:12:31 +0500

    Update and Delete

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -41,14 +41,61 @@ const getCourses = async () => {
   const pageNumber = 2;
   const pageSize = 10;
 
-  //   const findDoc = await Course.find()
-  //     .or([{ author: "Usama" }, { isPublished: true }])
-  //     .skip((pageNumber - 1) * pageSize)
-  //     .limit(pageSize)
-  //     .sort({ name: 1 });
-  //   // .select({ name: 1, tags: 1 });
-  //   // .countDocuments();
-  //   console.log(findDoc);
+  const findDoc = await Course.find()
+    .or([{ author: "Usama" }, { isPublished: true }])
+    .skip((pageNumber - 1) * pageSize)
+    .limit(pageSize)
+    .sort({ name: 1 });
+  // .select({ name: 1, tags: 1 });
+  // .countDocuments();
+  console.log(findDoc);
 };
 
 // getCourses();
+
+const getAllCourses = async () => {
+  const course = await Course.find();
+  console.log(course);
+};
+
+const updateCourse = async ({ id, status, authorName }) => {
+  const course = await Course.findById(id);
+  if (!course) return;
+  course.set({
+    isPublished: status,
+    author: authorName,
+  });
+
+  const result = await course.save();
+  console.log(result);
+};
+
+const updateFirst = async ({ id, status, authorName }) => {
+  const result = await Course.findByIdAndUpdate(
+    id,
+    {
+      $set: {
+        author: authorName,
+        isPublished: status,
+      },
+    },
+    { new: true }
+  );
+  console.log(result);
+};
+
+const newStatus = {
+  id: "65d892f736d2f54e241ec8ab",
+  status: true,
+  authorName: "Usama",
+};
+
+// updateCourse(newStatus);
+// updateFirst(newStatus);
+
+const deleteCourse = async ({ id }) => {
+  const result = await Course.deleteOne({ _id: id });
+  console.log(result);
+};
+
+deleteCourse(newStatus);
